08493bfa076ae0c9019abb3bdea8a95b2fb25de2
mehnoorsiddiqui <ayaankhan7532@gmail.com>
2023-04-15 22:40:25 +0500

    refactor : switch statements instead of if/else

--- handlers/handleListMessage.js
+++ handlers/handleListMessage.js
@@ -1,20 +1,27 @@
 const { sendMessage_ } = require("../services/WhatsAppCloudService");
 
 async function handleListMessage(from, message, userObj) {
-   
     const listReplyID = message?.interactive?.list_reply?.id;
-    if (listReplyID === "dalle") {
-        userObj.setModel("dalle");
-        await sendMessage_(from, "text", "You can now send messages to generate images");
-    } else if (listReplyID === "chatgpt") {
-        userObj.setModel("chatgpt");
-        await sendMessage_(from, "text", "You can now send messages to ChatGPT");
-    } else if (listReplyID === "ur") {
-        userObj.setLanguage("ur");
-        await sendMessage_(from, "text", "آپ اب اس کمپیوٹر پروگرام سے اردو زبان میں بات کرسکتے ہیں۔");
-    } else if (listReplyID === "en") {
-        userObj.setLanguage("en");
-        await sendMessage_(from, "text", "You can now talk to this bot in English");
+    
+    switch(listReplyID) {
+        case "dalle":
+            userObj.setModel("dalle");
+            await sendMessage_(from, "text", "You can now send messages to generate images");
+            break;
+        case "chatgpt":
+            userObj.setModel("chatgpt");
+            await sendMessage_(from, "text", "You can now send messages to ChatGPT");
+            break;
+        case "ur":
+            userObj.setLanguage("ur");
+            await sendMessage_(from, "text", "آپ اب اس کمپیوٹر پروگرام سے اردو زبان میں بات کرسکتے ہیں۔");
+            break;
+        case "en":
+            userObj.setLanguage("en");
+            await sendMessage_(from, "text", "You can now talk to this bot in English");
+            break;
+        default:
+            break;
     }
 }
 

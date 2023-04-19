a9349025dc3129e851bc2f07fe7acf54a6b9c27d
mehnoorsiddiqui <ayaankhan7532@gmail.com>
2023-04-19 20:39:18 +0500

    fix: translation prompt for dalle

--- handlers/handleTextMessage.js
+++ handlers/handleTextMessage.js
@@ -2,29 +2,31 @@ const { createChatcompletion, createImage } = require("../services/OpenAIService
 const { sendMessage } = require("../services/WhatsAppCloudService");
 
 async function handleTextMessage(from, message, userObj) {
-
     const textBody = message.text?.body;
     const botLanguage = userObj.getLanguage();
     const aiModel = userObj.getModel();
-    
+
     try {
         if (aiModel === "chatgpt") {
             const chatGPTResponse = await createChatcompletion(textBody, "normalPrompt", botLanguage);
             await sendMessage(from, "text", chatGPTResponse);
         } else if (aiModel === "dalle") {
-            const isEnglish = await createChatcompletion(textBody, "languageDetectPrompt");
-            if (isEnglish.toLowerCase().includes("yes")) {
-                console.log(textBody, "inside yes");
-                const dalleResponse = await createImage(textBody);
-                await sendMessage(from, "image", dalleResponse);
-            } else {
-                const translatedPromptInEnglish = await createChatcompletion(textBody, "translatePromptToEnglish");
-                const dalleResponse = await createImage(translatedPromptInEnglish);
-                await sendMessage(from, "image", dalleResponse);
-            }
+            const translatedPromptInEnglish = await createChatcompletion(textBody, "translatePromptToEnglish");
+            const regex = /"([^"]*)"/;
+            const match = regex.exec(translatedPromptInEnglish);
+            const dallePrompt = match ? match[1] : translatedPromptInEnglish;
+            const dalleResponse = await createImage(dallePrompt);
+            await sendMessage(from, "image", dalleResponse);
         }
+
     } catch (error) {
-        await sendMessage(from, "text", " Sorry, we could not complete your request at this time. Please try again later or contact support if the problem persists.");
+        console.log(error, "text");
+
+        await sendMessage(
+            from,
+            "text",
+            " Sorry, we could not complete your request at this time. Please try again later or contact support if the problem persists."
+        );
     }
 }
 
--- services/OpenAIService.js
+++ services/OpenAIService.js
@@ -4,7 +4,7 @@ const { Client, OpenAIController, FileWrapper } = require("openai-apilib");
 // Create OpenAI configuration
 const client = new Client({
   timeout: 0,
-  accessToken: process.env.OPENAI_API_KEY,
+  accessToken: process.env.OPENAI_API_KEY
 });
 
 // Create OpenAI API client
@@ -13,29 +13,32 @@ const openAIController = new OpenAIController(client);
 const createTranscription = async (audioFilePath, aiModel) => {
   const file = new FileWrapper(fs.createReadStream(audioFilePath));
   const model = "whisper-1";
-  const prompt = aiModel === "chatgpt" ? "English Language or اردو زبان" : "English Language";
+  const prompt =
+    aiModel === "chatgpt"
+      ? "English Language or اردو زبان"
+      : "English Language";
   const responseFormat = "json";
   const temperature = 0;
   const language = aiModel === "dalle" ? "en" : undefined;
   try {
-    const { result  } = await openAIController.createTranscription(file, model, prompt, responseFormat, temperature, language);    
+    const { result } = await openAIController.createTranscription(file,model,prompt,responseFormat,temperature,language);
     return result.text;
   } catch (error) {
     throw error;
   }
 };
 
-const createImage = async (prompt) => {
+const createImage = async prompt => {
   try {
     console.log(prompt, "inside createImage");
     const { result } = await openAIController.createImage({
       prompt: `${prompt}`,
       n: 1,
-      size: "256x256",
+      size: "256x256"
     });
     return result.data[0].url;
   } catch (error) {
-    throw error;    
+    throw error;
   }
 };
 
@@ -43,39 +46,34 @@ const createChatcompletion = async (promptMessage, promptType, language) => {
   const lang = language === "en" ? "English" : "Urdu";
   let systemPrompt, prompt;
 
-  if (promptType === "languageDetectPrompt") {
-    systemPrompt = `You are a helpful assistant that knows English. You have the ability to check if the sentence is in English or not.`;
-    prompt = `act as a  English language detector. Just detect if the following sentence is in English or not.The sentence is ${promptMessage}. Answer in YES or NO only. Do not write any explanations. `;
-  } else if (promptType === "translatePromptToEnglish") {
+  if (promptType === "translatePromptToEnglish") {
     systemPrompt = `You are a helpful assistant that translates sentences into English.`;
-    prompt = `translate [${promptMessage} ] to English. Do not include any description.  Just the translated sentence with quotations`;
+    prompt = `translate [${promptMessage}]  to English. Do not write any explanation or additional words with the translation. Only Send the translation in the quotation not any other information. 
+    If the message is already in English. Just send it in a quotation without explanation else send the translation. My first sentence/word is [${promptMessage} ].`;
   } else if (promptType == "normalPrompt") {
     systemPrompt = `You are ChatGPT, a large language model trained by OpenAI. The number of words in your answer should not exceed 50 words. Answer strictly in ${lang}`;
     prompt = `${promptMessage}`;
   }
-  console.log(systemPrompt);
-  console.log(prompt);
   try {
     const { result } = await openAIController.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
-          content: `${systemPrompt}`,
+          content: `${systemPrompt}`
         },
-        { role: "user", content: `${prompt}` },
+        { role: "user", content: `${prompt}` }
       ],
       temperature: 0.5,
       max_tokens: 500,
       top_p: 0.5,
       frequency_penalty: 0.5,
-      presence_penalty: 0.2,
+      presence_penalty: 0.2
     });
     return result.choices[0].message.content;
-  } catch (error) {    
-    throw error;        
+  } catch (error) {
+    throw error;
   }
 };
-
 
 module.exports = { createTranscription, createImage, createChatcompletion };
